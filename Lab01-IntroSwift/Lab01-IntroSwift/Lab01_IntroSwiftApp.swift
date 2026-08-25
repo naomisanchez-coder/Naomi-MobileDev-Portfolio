3a1a1760eeae705b7c927f7263a0bd0d95336551
Naomi Sanchez <naomi.sanchez@tecsup.edu.pe>
2026-08-25 17:47:28 -0500

    feat: Crear proyecto Lab01 y Playground en Semana01

---
+++ Lab01-IntroSwift/Lab01-IntroSwift/Lab01_IntroSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab01_IntroSwiftApp.swift
+//  Lab01-IntroSwift
+//
+//  Created by Tecsup on 24/08/26.
+//
+
+import SwiftUI
+
+@main
+struct Lab01_IntroSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
