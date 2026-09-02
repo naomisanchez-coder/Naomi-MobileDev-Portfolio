64130590207e47e9689fd1b89f306f16e2e2b99a
Naomi Sanchez <naomi.sanchez@tecsup.edu.pe>
2026-09-02 03:51:42 -0500

    feat: Crear proyecto Lab02 y Playground en Semana02

---
+++ Semana02/Lab02-Condicionales/Lab02-Condicionales/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Lab02-Condicionales
+//
+//  Created by NAOMI SANCHEZ on 31/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Semana02/Lab02-Condicionales/Lab02-Condicionales/Lab02_CondicionalesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab02_CondicionalesApp.swift
+//  Lab02-Condicionales
+//
+//  Created by NAOMI SANCHEZ on 31/08/26.
+//
+
+import SwiftUI
+
+@main
+struct Lab02_CondicionalesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
